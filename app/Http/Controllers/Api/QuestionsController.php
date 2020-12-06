31d78df89927509e77ecb44d480fbd56b412c256
hamstergeek <hamstergeek38@gmail.com>
2020-12-06 20:55:23 +0700

    create an endpoint to make question

--- app/Http/Controllers/Api/QuestionsController.php
+++ app/Http/Controllers/Api/QuestionsController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Question;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Http\Requests\AskQuestionRequest;
 use App\Http\Resources\QuestionsResource;
 
 class QuestionsController extends Controller
@@ -29,9 +30,14 @@ class QuestionsController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(AskQuestionRequest $request)
     {
-        //
+        $question = $request->user()->questions()->create($request->only('title', 'body'));
+
+        return response()->json([
+            'message' => 'Your questions has been submitted',
+            'question' => new QuestionsResource($question)
+        ]);
     }
 
     /**
--- routes/api.php
+++ routes/api.php
@@ -14,9 +14,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
 Route::get('questions', 'Api\QuestionsController@index');
+
+
 Route::post('token', 'Auth\LoginController@getToken');
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::middleware('auth:api')->group(function(){
+    Route::apiResource('questions', 'Api\QuestionsController')->except('index');
 });
